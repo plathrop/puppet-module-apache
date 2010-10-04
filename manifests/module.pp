0bf8ed5b19221f05f1b4fe3b6cff9b9077ad1006
Ian Eure <ian.eure@gmail.com>
2010-10-04 14:39:06 -0700

    Use the correct variable name. This was causing a2enmod to run every time Puppet did, which caused Apache to reload every time as well.

--- manifests/module.pp
+++ manifests/module.pp
@@ -2,14 +2,14 @@ define apache::module($ensure = 'present') {
   case $ensure {
     present,installed : {
       exec { "/usr/sbin/a2enmod $name":
-        creates => "${apache::mods_enabled}/${name}.load",
+        creates => "${apache::apache_mods_enabled}/${name}.load",
   	notify  => Service["apache"],
         require => Package["apache"],
       }
     }
     absent,purged: {
       exec { "/usr/sbin/a2dismod $name":
-        onlyif  => "/usr/bin/test -L ${apache::mods_enabled}/${name}.load",
+        onlyif  => "/usr/bin/test -L ${apache::apache_mods_enabled}/${name}.load",
   	notify  => Service["apache"],
         require => Package["apache"],
       }
