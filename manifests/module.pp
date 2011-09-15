f850029b59a4dcbca4f67604d982fb38699d13f0
Geoff Crompton <geoffc@trinity.unimelb.edu.au>
2011-09-15 11:33:09 +1000

    add 'package' argument to apache::module definition
    It can be useful to specify what package provides an apache module at the
    same time as declaring the apache module. For instance:
    
     apache::module { "php5":
         ensure => present,
         package => "libapache2-mod-php5",
     }

--- manifests/module.pp
+++ manifests/module.pp
@@ -1,4 +1,4 @@
-define apache::module($ensure = 'present') {
+define apache::module($ensure = 'present', $package=undef) {
   case $ensure {
     present,installed : {
       exec { "/usr/sbin/a2enmod $name":
@@ -6,12 +6,22 @@ define apache::module($ensure = 'present') {
   	notify  => Service["apache"],
         require => Package["apache"],
       }
+      if $package {
+          package { $package:
+              ensure => installed,
+          }
+      }
     }
     absent,purged: {
       exec { "/usr/sbin/a2dismod $name":
         onlyif  => "/usr/bin/test -L ${apache::apache_mods_enabled}/${name}.load",
   	notify  => Service["apache"],
         require => Package["apache"],
+      }
+      if $package {
+          package { $package:
+              ensure => absent,
+          }
       }
     }
     default: { err ( "apache::module Unknown ensure value: '$ensure'" ) }
